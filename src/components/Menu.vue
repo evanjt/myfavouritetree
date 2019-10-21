5fd564dcf750d3660ee62afdb870289cc5ca3c05
Evan Thomas <evan@evanjt.com>
2019-10-21 13:07:34 +0200

    Menu fixed

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="menu">
     <ul>
-      <li v-for="entry in menuEntries" :key="entry">
+      <li
+        v-for="entry in menuEntries"
+        :key="entry.id"
+        :class="{ 'active-entry': activeEntries.includes(entry.id) }"
+      >
         <a href="#" @click.prevent="entryCliked(entry)">
-          <span :class="{ 'active-entry': entry === activeEntry }">{{entry}}</span>
+          <span>{{ entry.title }}</span>
         </a>
       </li>
     </ul>
@@ -14,18 +18,27 @@
 export default {
   props: {
     menuEntries: Array,
-    activeEntry: String
+    activeEntries: Array
   },
   methods: {
     entryCliked(entry) {
-      this.$emit("activeEntryChanged", entry);
+      this.$emit("activeEntryChanged", entry.id);
     }
   }
 };
 </script>
 
 <style scoped>
+.menu ul {
+  list-style-type: none;
+}
+.menu ul li {
+  line-height: 1.5em;
+}
 .active-entry {
+  list-style-type: square;
+}
+.active-entry span {
   font-weight: bold;
 }
 </style>
